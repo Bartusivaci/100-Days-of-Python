ea95bdeedc5257079bcf084dbeeabbe47c7243af
Bartu Sıvacı <bartusivaci@gmail.com>
2022-12-15 13:40:16 +0100

    Day 6 is completed.

---
+++ Day 6 - Escaping The Maze/main.py
@@ -0,0 +1,22 @@
+# This assignment was made for Reeborg's World to escape the maze
+# The code is commented to avoid compiler errors
+
+'''
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+
+'''
